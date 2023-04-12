742540887dd28760a5093dad8b293b89a8592515
Ramazan Mert Kaya <kmert0@outlook.com>
2023-04-12 12:22:20 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+// don't change the Component name "App"
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounterHandler() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p id="counter">{counter}</p>
+      <button onClick={incrementCounterHandler}>Increment</button>
     </div>
   );
 }
-
-export default App;
